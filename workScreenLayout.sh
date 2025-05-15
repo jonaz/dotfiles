160fbc8e029f544957949e914575652a976b2a0c
Jonas Falck <jonas.falck@fortnox.se>
2025-05-15 15:05:04 +0200

    make i3 quiet

--- workScreenLayout.sh
+++ workScreenLayout.sh
@@ -5,10 +5,10 @@ CURRENT_WORKSPACE=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==tru
 PRIMARY=$1
 SECONDARY=$2
 
-i3-msg "workspace 1; move workspace to output $1"
-i3-msg "workspace 2; move workspace to output eDP"
+i3-msg -q "workspace 1; move workspace to output $1"
+i3-msg -q "workspace 2; move workspace to output eDP"
 for i in {3..10}; do
-	i3-msg "workspace $i; move workspace to output $2"
+	i3-msg -q "workspace $i; move workspace to output $2"
 done
 
-i3-msg "workspace $CURRENT_WORKSPACE"
+i3-msg -q "workspace $CURRENT_WORKSPACE"
