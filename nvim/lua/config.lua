cedc66a4a379f2d28ad083e4b21ff2ab7b37efe1
Jonas Falck <jonas.falck@fortnox.se>
2025-12-15 12:54:12 +0100

    fix broken stuff

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -101,7 +101,8 @@ require('lualine').setup({
 
 require "fidget".setup {} -- lsp loading info
 require("nvim-surround").setup({})
-require('leap').add_default_mappings()
+vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-backward)')
 
 cmp.setup({
 	preselect = cmp.PreselectMode.None,
@@ -278,9 +279,68 @@ for server, opts in pairs(servers) do
 end
 
 -- treesitter stuff
-require('nvim-treesitter.configs').setup {
-	-- Add languages to be installed here that you want installed for treesitter
-	ensure_installed = {
+-- require('nvim-treesitter').setup {
+-- 	highlight = {
+-- 		enable = true,
+-- 		additional_vim_regex_highlighting = false,
+-- 	},
+-- 	indent = { enable = true, disable = { 'python' } },
+-- 	incremental_selection = {
+-- 		enable = true,
+-- 		keymaps = {
+-- 			init_selection = '<c-space>',
+-- 			node_incremental = '<c-space>',
+-- 			scope_incremental = '<c-s>',
+-- 			node_decremental = '<c-backspace>',
+-- 		},
+-- 	},
+-- 	textobjects = {
+-- 		select = {
+-- 			enable = true,
+-- 			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+-- 			keymaps = {
+-- 				-- You can use the capture groups defined in textobjects.scm
+-- 				['aa'] = '@parameter.outer',
+-- 				['ia'] = '@parameter.inner',
+-- 				['af'] = '@function.outer',
+-- 				['if'] = '@function.inner',
+-- 				['ac'] = '@class.outer',
+-- 				['ic'] = '@class.inner',
+-- 			},
+-- 		},
+-- 		move = {
+-- 			enable = true,
+-- 			set_jumps = true, -- whether to set jumps in the jumplist
+-- 			goto_next_start = {
+-- 				[']m'] = '@function.outer',
+-- 				[']]'] = '@class.outer',
+-- 			},
+-- 			goto_next_end = {
+-- 				[']M'] = '@function.outer',
+-- 				[']['] = '@class.outer',
+-- 			},
+-- 			goto_previous_start = {
+-- 				['[m'] = '@function.outer',
+-- 				['[['] = '@class.outer',
+-- 			},
+-- 			goto_previous_end = {
+-- 				['[M'] = '@function.outer',
+-- 				['[]'] = '@class.outer',
+-- 			},
+-- 		},
+-- 		swap = {
+-- 			enable = true,
+-- 			swap_next = {
+-- 				['<leader>a'] = '@parameter.inner',
+-- 			},
+-- 			swap_previous = {
+-- 				['<leader>A'] = '@parameter.inner',
+-- 			},
+-- 		},
+-- 	},
+-- }
+
+require'nvim-treesitter'.install{
 		'dockerfile',
 		'c',
 		'cpp',
@@ -305,68 +365,12 @@ require('nvim-treesitter.configs').setup {
 		'http',
 		'java',
 		'groovy',
-	},
-
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = { enable = true, disable = { 'python' } },
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = '<c-space>',
-			node_incremental = '<c-space>',
-			scope_incremental = '<c-s>',
-			node_decremental = '<c-backspace>',
-		},
-	},
-	textobjects = {
-		select = {
-			enable = true,
-			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-			keymaps = {
-				-- You can use the capture groups defined in textobjects.scm
-				['aa'] = '@parameter.outer',
-				['ia'] = '@parameter.inner',
-				['af'] = '@function.outer',
-				['if'] = '@function.inner',
-				['ac'] = '@class.outer',
-				['ic'] = '@class.inner',
-			},
-		},
-		move = {
-			enable = true,
-			set_jumps = true, -- whether to set jumps in the jumplist
-			goto_next_start = {
-				[']m'] = '@function.outer',
-				[']]'] = '@class.outer',
-			},
-			goto_next_end = {
-				[']M'] = '@function.outer',
-				[']['] = '@class.outer',
-			},
-			goto_previous_start = {
-				['[m'] = '@function.outer',
-				['[['] = '@class.outer',
-			},
-			goto_previous_end = {
-				['[M'] = '@function.outer',
-				['[]'] = '@class.outer',
-			},
-		},
-		swap = {
-			enable = true,
-			swap_next = {
-				['<leader>a'] = '@parameter.inner',
-			},
-			swap_previous = {
-				['<leader>A'] = '@parameter.inner',
-			},
-		},
-	},
 }
 
 -- require("rest-nvim").setup({
 -- 	env_file = '.requests.env'
 -- })
+--
+
+
+
